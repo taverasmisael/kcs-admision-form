842588dbe4e2885a9b3ddd0447eeb7a59f712291
taverasmisael <macrot29@gmail.com>
2018-01-20 01:43:05 -0400

    refact(APP): restructure component to use JSS and render only one component for now

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import Intro from '../components/Intro'
 import Footer from '../components/Footer'
 import AdmisionForm from '../AdmisionForm'
+import ChildrenInfo from '../steps/ChildrenInfo'
 
+import withStyles from 'material-ui/styles/withStyles'
+import styles from './styles'
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Intro />
-        <AdmisionForm />
+        <main className={this.props.classes.container}>
+          <ChildrenInfo />
+        </main>
         <Footer />
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default withStyles(styles)(App)
