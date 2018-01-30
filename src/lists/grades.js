0b5051cf90497b27a509e94dad9f78a9114f4b2f
taverasmisael <macrot29@gmail.com>
2018-01-30 11:12:54 -0400

    feat(grades): use real KCS grades

--- src/lists/grades.js
+++ src/lists/grades.js
@@ -1,9 +1,18 @@
 export default [
-  { value: 'Kinder', label: 'Kinder' },
-  { value: 'Primero', label: 'Primero' },
-  { value: 'Segundo', label: 'Segundo' },
-  { value: 'Tercero', label: 'Tercero' },
-  { value: 'Cuarto', label: 'Cuarto' },
-  { value: 'Quinto', label: 'Quinto' },
-  { value: 'Sexto', label: 'Sexto' }
+  { value: 'N2', label: 'N2' },
+  { value: 'N3', label: 'N3' },
+  { value: 'K4', label: 'K4' },
+  { value: 'k5', label: 'k5' },
+  { value: '1st', label: '1st' },
+  { value: '2nd', label: '2nd' },
+  { value: '3rd', label: '3rd' },
+  { value: '4th', label: '4th' },
+  { value: '5th', label: '5th' },
+  { value: '6th', label: '6th' },
+  { value: '7th', label: '7th' },
+  { value: '8th', label: '8th' },
+  { value: '9th', label: '9th' },
+  { value: '10th', label: '10th' },
+  { value: '11th', label: '11th' },
+  { value: '12th', label: '12th' }
 ]
